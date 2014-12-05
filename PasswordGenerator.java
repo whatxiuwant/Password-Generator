d5f67c3cfe839eb7808f33dc4d9f377183ea22e5
whatxiuwant <yimmierr@gmail.com>
2014-12-05 14:40:06 -0500

    Final Version
    added front end

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,40 +1,34 @@
 package pgpkg;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
 
 public class PasswordGenerator {
-/*
-	(0) create empty string to store password
-	(1) generate random number between 8 and 12 inclusive, to be used as the length of the password
-	(2) create char array of 62 elements: A-Z, a-z, 0-9
-	(3) create method getAlpha()
-	(4) create method getAlphaNumeric()
-	(5) call getAlpha() for firstCharacter
-	(6) call getAlphaNumeric() 7-11 times, depending on the length of the password to be generated, call it password.length() - 1, so if its 8 characters long, call it 7 times
-	(7) randomly select "_" or "-"
-	(8) randomly place in random positions 2 - password.length(), replacing the character at that position
-	(9) check requirements: 1 upper, 1 lower, 1 numeric; if not repeat process
-	(10) return generated password
-*/
 	static String password = "";
-	static int length;
-	static char[] characters = new char[62];
+	static char[] characters = new char[62];		//char array of all possible pw characters
+	static int length = 0;
 	
 	public static void main(String[] args) {
 		characters = generateAlphaNumericArray();
-		System.out.println(characters);
 		
 		password = generatePw();
-		System.out.println(password);
 		
-		if (!isValidPassword(password))
+		if (!isValidPw(password)) {
+			password = "";
 			password = generatePw();
-		System.out.println(password);
+		}
+		
+		UIManager.put("OptionPane.messageFont", new FontUIResource(new Font("SanSerif", Font.PLAIN, 20)));
+		UIManager.put("OptionPane.messageForeground", Color.BLACK);
+		JOptionPane.showMessageDialog(null,"The password is: " + password + "\nThe password length is: " + length);
 	}
 	
 	public static String generatePw() {
 		length = generatePwLength();
 		password += getAlpha();
 		
-		for (int i = 0; i < length - 1; i++)
+		for (int i = 0; i < length - 2; i++)
 			password += getAlphaNumeric();
 		
 		String sc = getSpecialCase();
@@ -56,7 +50,7 @@ public class PasswordGenerator {
 	}
 	
 	public static int generatePwLength() {
-		return (int) Math.random() * 5 + 8;
+		return (int) (Math.random() * 5 + 8);
 	}
 	
 	public static char getAlpha() {
@@ -71,6 +65,7 @@ public class PasswordGenerator {
 		for (int i = 0; i < length - 1; i++)
 			if (pw.charAt(i) >= 'a' && pw.charAt(i) <= 'z')
 				return true;
+		
 		return false;
 	}
 	
@@ -78,6 +73,7 @@ public class PasswordGenerator {
 		for (int i = 0; i < length - 1; i++)
 			if (pw.charAt(i) >= 'A' && pw.charAt(i) <= 'Z')
 				return true;
+		
 		return false;
 	}
 	
@@ -85,11 +81,12 @@ public class PasswordGenerator {
 		for (int i = 0; i < length - 1; i++)
 			if (pw.charAt(i) >= '0' && pw.charAt(i) <= '9')
 				return true;
+		
 		return false;
 	}
 	
 	public static String getSpecialCase() {
-		int random = (int) Math.random() * 2;
+		int random = (int) (Math.random() * 2);
 		
 		if (random == 0)
 			return "_";
@@ -109,6 +106,7 @@ public class PasswordGenerator {
 	public static boolean isValidPw(String pw) {
 		if (hasLower(pw) && hasUpper(pw) && hasNumeric(pw))
 			return true;
+		
 		return false;
 	}
 }
